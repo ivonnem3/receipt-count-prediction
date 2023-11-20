b296abfeda3e31b042cafa24ea06ea36de187e8c
ivonnem3 <ivonneam0925@gmail.com>
2023-11-20 14:03:30 -0600

    rc_forcasting fine tunning

--- rc_forcasting.py
+++ rc_forcasting.py
@@ -17,7 +17,6 @@ reciept_data = pd.read_csv('data\data_daily.csv')
 reciept_data = reciept_data.rename(columns={'# Date':'Date'})
 reciept_data.index = pd.to_datetime(reciept_data['Date'],format='%Y-%m-%d')
 del reciept_data['Date']
-print(reciept_data)
 
 # Train-Test Split
 train =  reciept_data[reciept_data.index <= pd.to_datetime("2021-10-19", format='%Y-%m-%d')]
@@ -34,12 +33,28 @@ plt.xticks(rotation=45)
 plt.show()
 """
 
-# Generate Forcasting Model
-from statsmodels.tsa.statespace.sarimax import SARIMAX
-y = train['Receipt_Count']
-SARIMAXmodel = SARIMAX(y, order = (0,1 , 2), seasonal_order=(0,1,1,12))
+# Generate Forcasting Model & Predictions
+SARIMAXmodel = SARIMAX(reciept_data.Receipt_Count, order = (0,1,2), seasonal_order=(0,1,1,12))
 SARIMAXmodel = SARIMAXmodel.fit()
 
+start = len(train)
+end = len(reciept_data)-1
+predictions = SARIMAXmodel.predict(start=start,end = end, dynamic=True).rename('Predicted')
+
+arma_rmse = np.sqrt(mean_squared_error(test["Receipt_Count"].values, predictions ))
+print("RMSE: ",arma_rmse)
+
+plt.plot(train,color = 'yellowgreen', label = 'Train Data')
+plt.plot(test, color = 'crimson', label = 'Test Data')
+plt.title('Reciept Count')
+plt.xlabel('Date')
+plt.ylabel('Reciet Count')
+plt.xticks(rotation=45)
+plt.plot(predictions, color='green', label = 'Predictions')
+plt.legend()
+plt.show()
+
+"""
 y_pred = SARIMAXmodel.get_forecast(len(test.index))
 y_pred_df = y_pred.conf_int(alpha = 0.05)
 y_pred_df["Predictions"] = SARIMAXmodel.predict(start = y_pred_df.index[0], end = y_pred_df.index[-1])
@@ -54,8 +69,8 @@ plt.xticks(rotation=45)
 plt.plot(y_pred_out, color='green', label = 'Predictions')
 plt.legend()
 plt.show()
-arma_rmse = np.sqrt(mean_squared_error(test["Receipt_Count"].values, y_pred_df["Predictions"]))
-print("RMSE: ",arma_rmse)
+"""
+
 
 #reciept_model = SARIMAX(train['Receipt_Count'])
 #reciept_model = reciept_model.fit()
